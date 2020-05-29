061b9bd867c15177ea7fd6b15da1f9683496641a
FrankFlores <fraco.1918@gmail.com>
2020-05-29 11:42:13 -0500

    install devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # associations
   has_many :memes
   has_and_belongs_to_many :voted_memes, class_name: "Meme"
