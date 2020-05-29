1ff861ebfa6c17f865286290506f337257d43500
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-29 16:07:49 -0500

    update memes_by_category view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,10 @@ class HomeController < ApplicationController
   end
 
   def memes_by_category
-    
+    @memes_by_category = Meme.all.group_by { |meme| meme.category.name }
   end
-  
+
   def memes_by_popularity
-    
+
   end
 end
