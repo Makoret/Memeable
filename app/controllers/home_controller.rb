55c7cae34c836f0b5d819d01e0f474e564577bab
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-29 19:55:45 -0500

    add meme details view and link from any page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,18 @@
 class HomeController < ApplicationController
   def index
-    result = Meme.all
-    @memes_by_date = result.group_by { |meme| meme.created_at.to_date }
+    memes_by_date = Meme.all.group_by do
+      |meme| meme.created_at.to_date.strftime("%A, %B %d")
+    end
+    @memes = memes_by_date
   end
 
   def memes_by_category
-    @memes_by_category = Meme.all.group_by { |meme| meme.category.name }
+    memes_by_category = Meme.all.group_by { |meme| meme.category.name }
+    @memes = memes_by_category
   end
 
   def memes_by_popularity
-    @popular_memes = Meme.all.order(:votes_count)
+    memes_by_popularity = Meme.all.order(:votes_count)
+    @memes = { 'Most popular memes' => memes_by_popularity }
   end
 end
